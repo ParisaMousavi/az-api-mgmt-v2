50399a989daf72f817c0821230966ce2425db98d
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-10-17 22:29:23 +0200

    update

--- output.tf
+++ output.tf
@@ -6,6 +6,6 @@ output "id" {
   value = azurerm_api_management.this.id
 }
 
-output "fqdn" {
-  value = azurerm_private_dns_a_record.dns_record.fqdn
-}
+# output "fqdn" {
+#   value = azurerm_private_dns_a_record.dns_record.fqdn
+# }
